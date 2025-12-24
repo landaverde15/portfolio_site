90f429fb5b54a85aa6ddc93743cd5b072c3805cf
Erik Landaverde <landaverde.erik10@gmail.com>
2025-12-23 19:57:24 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import './App.css';
 import Landing from './Pages/Landing';
 import Header from './ReusableComponents/Header';
-import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header></Header>
-        <Routes>
-            <Route path="/" element={<Landing/>}></Route>
-        </Routes>
+        <Landing></Landing>
       </div>
     </div>
   );
